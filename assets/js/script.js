bfeac9fb8c7bbf13575d4824a183e586f9d602dc
carlaamt <car.moyat@duocuc.cl>
2023-03-17 17:07:19 -0300

    practice

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,5 @@ var menu = document.getElementById('menu-mobile');
 var nav = document.getElementById('nav-content');
 menu.addEventListener('click', function(){
     nav.classList.toggle('nav-mobile');
+    menu.classList.toggle('btn-transform');
 })
